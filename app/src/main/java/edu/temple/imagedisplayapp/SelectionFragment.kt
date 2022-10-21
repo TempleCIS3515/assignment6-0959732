791246a02098ebf205facf83bfd74a06d1120ecd
NabaaAbdulhussein <111922494+NabaaAbdulhussein@users.noreply.github.com>
2022-10-21 15:40:39 -0400

    commit 10/21/22 3PM

--- app/src/main/java/edu/temple/imagedisplayapp/SelectionFragment.kt
+++ app/src/main/java/edu/temple/imagedisplayapp/SelectionFragment.kt
@@ -32,24 +32,27 @@ class SelectionFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_selection, container, false).also {
+            recyclerView = it.findViewById(R.id.recyclerView)
+        }
 
-            recyclerView = it.findViewById(R.id.recyclerView) }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
         super.onViewCreated(view, savedInstanceState)
 
                 val clickEvent = {
                         image:Image -> imageViewModel.setSelectedImage(image)
+
                 }
+
                 recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
                 recyclerView.adapter = ImageAdapter(image, clickEvent)
+
             }
 
     companion object {
